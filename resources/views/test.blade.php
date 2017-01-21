8ab9ca09c17d3134dc1d71d6596cb78e8c23d076
朱明鉴 <1532811985@qq.com>
2017-01-21 18:56:15 +0800

    开发环境配置完毕

---
+++ resources/views/test.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+    {{--<link rel="stylesheet" href="{{asset('css/app.css')}}">--}}
+    <link rel="stylesheet" href="{{asset('bootstrap/dist/css/bootstrap.min.css')}}">
+</head>
+<body>
+test.blade.php
+<br>
+<button type="button" class="btn btn-link">按钮</button>
+
+<p>
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+    <button type="button" class="btn btn-default btn-lg">Large button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-danger">Default button</button>
+    <button type="button" class="btn btn-default">Default button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-primary btn-sm">Small button</button>
+    <button type="button" class="btn btn-default btn-sm">Small button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-primary btn-xs">Extra small button</button>
+    <button type="button" class="btn btn-default btn-xs">Extra small button</button>
+</p><p>
+    <button type="button" class="btn btn-primary btn-lg">Large button</button>
+    <button type="button" class="btn btn-default btn-lg">Large button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-primary">Default button</button>
+    <button type="button" class="btn btn-default">Default button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-primary btn-sm">Small button</button>
+    <button type="button" class="btn btn-default btn-sm">Small button</button>
+</p>
+<p>
+    <button type="button" class="btn btn-primary btn-xs">Extra small button</button>
+    <button type="button" class="btn btn-default btn-xs">Extra small button</button>
+</p>
+
+
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
